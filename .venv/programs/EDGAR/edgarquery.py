ec06264c8a27057175dbe09fe06429c87053d536
unit0113 <unit0113@gmail.com>
2021-06-22 19:48:08 +0300

    Major overhaul of parse fillings, 90% complete for single stock check

--- .venv/programs/EDGAR/edgarquery.py
+++ .venv/programs/EDGAR/edgarquery.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 import sys
+import re
 
 
 class StockData:
@@ -176,31 +177,187 @@ def annualData(stock, headers, max_attempts = 5):
 
 def parse_filings(filings, stock, type, headers, max_attempts = 5):
     
-    # Define statements to Parse
-    item1 = r'INCOME STATEMENTS'
-    item2 = r'BALANCE SHEETS'
-    item3 = r'CASH FLOWS STATEMENTS'
-    item4 = r'CONSOLIDATED STATEMENT OF OPERATIONS'
-    item5 = r'DIVIDENDS DECLARED (DETAIL)'
-    item6 = r'BASIC AND DILUTED EARNINGS PER SHARE (DETAIL)'
-    item7 = r'CONSOLIDATED STATEMENTS OF CASH FLOWS'
-    item8 = r'CONSOLIDATED STATEMENTS OF OPERATIONS'
-    item9 = r'CONSOLIDATED BALANCE SHEETS'
-    item10 = r'STATEMENT OF CASH FLOWS INDIRECT'
-    item11 = r'STATEMENT OF INCOME'
-    item12 = r'STATEMENT OF FINANCIAL POSITION CLASSIFIED'
-    item13 = r'CONSOLIDATED BALANCE SHEET'
-    item14 = r'CONSOLIDATED STATEMENT OF CASH FLOWS'
-    item15 = r'CONSOLIDATED STATEMENTS OF EARNINGS'
-    item16 = r'CONSOLIDATED STATEMENT OF INCOME'
-    item17 = r'CONSOLIDATED STATEMENTS OF INCOME'
-    item18 = r'STATEMENTS OF CONSOLIDATED INCOME'
-    item19 = r'STATEMENTS OF CONSOLIDATED CASH FLOWS'
-    item20 = r'STATEMENT OF INCOME ALTERNATIVE'
-    report_list = [item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13, item14, item15, item16, item17, item18, item19, item20]
-    statements_url = []
+    # Pull fiscal period from .htm
+    def fy_html(soup):
+        fy = '---'
+        for row in soup.table.find_all('tr'):
+            tds = row.find_all('td')
+            if 'DocumentFiscalYearFocus' in str(tds):
+                fy = int(''.join(re.findall(r'\d+', tds[1].text.strip())))
+                return fy
 
-    for filing in filings:
+
+    # Pull fiscal period from .htm
+    def fy_xml(soup):
+        fy = '---'
+        rows = soup.find_all('Row')
+        for row in rows:
+            if 'DocumentFiscalYearFocus' in str(row):
+                for cell in row:
+                    for line in str(cell).split('\n'):
+                        if 'NonNumbericText' in str(line):
+                            fy = int(re.findall(r'\d\d\d\d', line.strip())[0])
+                            return fy
+
+
+    # Pull income data
+    def rev_html(soup):
+        rev = gross = oi = net = eps = '---'
+        rev_check = 0
+        cost_check = 0
+        for row in soup.table.find_all('tr'):
+            tds = row.find_all('td')
+            if 'defref_us-gaap_Revenue' in str(tds) and rev_check == 0 or 'this, \'defref_us-gaap_SalesRevenueNet\', window' in str(tds) and rev_check == 0:
+                rev = int(''.join(re.findall(r'\d+', tds[1].text.strip())))
+                rev_check = 1
+            elif 'defref_us-gaap_GrossProfit' in str(tds):
+                gross = int(''.join(re.findall(r'\d+', tds[1].text.strip())))
+            elif 'defref_us-gaap_OperatingIncomeLoss' in str(tds):
+                oi = int(''.join(re.findall(r'\d+', tds[1].text.strip())))
+            elif 'defref_us-gaap_NetIncomeLoss' in str(tds):
+                net = int(''.join(re.findall(r'\d+', tds[1].text.strip())))
+            elif 'EarningsPerShareDiluted' in str(tds):
+                eps = float(''.join(re.findall(r'\d+\.\d+', tds[1].text.strip())))
+            elif 'this, \'defref_us-gaap_CostOfRevenue\', window' in str(tds) and cost_check == 0 or 'this, \'defref_us-gaap_CostOfGoodsAndServicesSold\', window' in str(tds) and cost_check == 0:
+                cost = int(''.join(re.findall(r'\d+', tds[1].text.strip())))
+
+        # Calculate gross if not found
+        if gross == '---':
+            gross = rev - cost
+
+        return rev, gross, oi, net, eps
+    
+
+    # Pull income data
+    def rev_xml(soup):
+        rev = gross = oi = net = eps = '---'
+        rows = soup.find_all('Row')
+        for row in rows:
+            if 'us-gaap_Revenues' in str(row):
+                for cell in row:
+                    for line in str(cell).split('\n'):
+                        if 'RoundedNumericAmount' in str(line):
+                            rev = int(re.findall(r'\d+', line.strip())[0])
+                            break
+            elif 'us-gaap_CostOfRevenue' in str(row):
+                for cell in row:
+                    for line in str(cell).split('\n'):
+                        if 'RoundedNumericAmount' in str(line):
+                            cost = int(re.findall(r'\d+', line.strip())[0])
+                            break                
+                gross = rev - cost
+            elif 'us-gaap_OperatingIncomeLoss' in str(row):
+                for cell in row:
+                    for line in str(cell).split('\n'):
+                        if 'RoundedNumericAmount' in str(line):
+                            oi = int(re.findall(r'\d+', line.strip())[0])
+                            break 
+            elif 'us-gaap_NetIncomeLoss' in str(row):
+                for cell in row:
+                    for line in str(cell).split('\n'):
+                        if 'RoundedNumericAmount' in str(line):
+                            net = int(re.findall(r'\d+', line.strip())[0])
+                            break                
+            elif 'us-gaap_EarningsPerShareDiluted' in str(row):
+                for cell in row:
+                    for line in str(cell).split('\n'):
+                        if 'RoundedNumericAmount' in str(line):
+                            eps = float(''.join(re.findall(r'\d+\.\d\d', line.strip())))
+                            break
+        
+        return rev, gross, oi, net, eps 
+
+
+    # Pull balance sheet data
+    def bs_html(soup):
+        cash = goodwill = assets = liabilities = '---'
+        for row in soup.table.find_all('tr'):
+            tds = row.find_all('td')
+            if 'this, \'defref_us-gaap_CashAndCashEquivalentsAtCarryingValue\', window' in str(tds):
+                for td in tds:
+                    if 'nump' in str(td):
+                        cash = int(''.join(re.findall(r'\d+', td.text.strip())))
+                        break
+            elif 'defref_us-gaap_Goodwill' in str(tds):
+                for td in tds:
+                    if 'nump' in str(td):
+                        goodwill = int(''.join(re.findall(r'\d+', td.text.strip())))
+                        break
+            elif 'Total assets' in str(tds):
+                for td in tds:
+                    if 'nump' in str(td):
+                        assets = int(''.join(re.findall(r'\d+', td.text.strip())))
+                        break
+            elif 'onclick="top.Show.showAR( this, \'defref_us-gaap_Liabilities\', window )' in str(tds):
+                for td in tds:
+                    if 'nump' in str(td):
+                        liabilities = int(''.join(re.findall(r'\d+', td.text.strip())))
+                        break
+    
+        # Remove goodwill from assets
+        assets -= goodwill
+
+        return cash, assets, liabilities
+
+    # Pull cash flow data
+    def cf_html(soup):
+        cfo = capex = buyback = divpaid = '---'
+        for row in soup.table.find_all('tr'):
+            tds = row.find_all('td')
+            if 'Net cash from operations' in str(tds):
+                cfo = int(''.join(re.findall(r'\d+', tds[1].text.strip())))
+            elif 'Additions to property and equipment' in str(tds):
+                capex = int(''.join(re.findall(r'\d+', tds[1].text.strip())))
+            elif 'Common stock repurchased' in str(tds):
+                buyback = int(''.join(re.findall(r'\d+', tds[1].text.strip())))
+            elif 'Common stock cash dividends paid' in str(tds):
+                divpaid = int(''.join(re.findall(r'\d+', tds[1].text.strip())))
+        
+        # Calculate FCF
+        try:
+            fcf = cfo - capex
+        except:
+            fcf = '---'
+
+        return fcf, buyback, divpaid
+
+    # Pull div data
+    def div_html(soup):
+        div = '---'
+        for row in soup.table.find_all('tr'):
+            tds = row.find_all('td')
+            if 'onclick="top.Show.showAR( this, \'defref_us-gaap_DividendsPayableDateDeclaredDayMonthAndYear\', window )' in str(tds):
+                for index, row in enumerate(tds):
+                    if not re.findall(r'\d', str(row)):
+                        break
+            elif 'onclick="top.Show.showAR( this, \'defref_us-gaap_CommonStockDividendsPerShareDeclared\', window )' in str(tds):
+                div = float(''.join(re.findall(r'\d+\.\d+', tds[index].text.strip())))
+
+        return div
+
+    # Define statements to Parse
+    intro_list = r'DOCUMENT AND ENTITY INFORMATION', 
+    income_list = r'CONSOLIDATED STATEMENTS OF EARNINGS', r'STATEMENT OF INCOME ALTERNATIVE', r'CONSOLIDATED STATEMENT OF INCOME', r'INCOME STATEMENTS', r'STATEMENT OF INCOME', r'CONSOLIDATED STATEMENTS OF OPERATIONS', r'STATEMENTS OF CONSOLIDATED INCOME', r'CONSOLIDATED STATEMENTS OF INCOME', r'CONSOLIDATED STATEMENT OF OPERATIONS'
+    bs_list = r'BALANCE SHEETS', r'CONSOLIDATED BALANCE SHEETS', r'STATEMENT OF FINANCIAL POSITION CLASSIFIED', r'CONSOLIDATED BALANCE SHEET'
+    cf_list = r'CASH FLOWS STATEMENTS', r'CONSOLIDATED STATEMENTS OF CASH FLOWS', r'STATEMENT OF CASH FLOWS INDIRECT', r'CONSOLIDATED STATEMENT OF CASH FLOWS', r'STATEMENTS OF CONSOLIDATED CASH FLOWS'
+    div_list = r'DIVIDENDS DECLARED (DETAIL)', 
+
+    # Lists for data frame
+    Fiscal_Period = []
+    Revenue = []
+    Gross_Profit = []
+    Operating_Income = []
+    Net_Profit = []
+    Earnings_Per_Share = []
+    Cash = []
+    Total_Assets = []
+    Total_Liabilities = []
+    Free_Cash_Flow = []
+    Share_Buybacks = []
+    Dividend_Payments = []
+    Dividends = []
+    
+    for filing in filings: 
         for i in range(max_attempts):
             try:
                 content = requests.get(filing['documents'], headers=headers).json()
@@ -214,84 +371,127 @@ def parse_filings(filings, stock, type, headers, max_attempts = 5):
             sys.exit()
         
         for file in content['directory']['item']:  
-            # Grab the filing summary and create a new url leading to the file so we can download it.
+            # Grab the filing summary url
             if file['name'] == 'FilingSummary.xml':
                 xml_summary = r"https://www.sec.gov" + content['directory']['name'] + "/" + file['name']
+                print(xml_summary)
                 
-                # Define a new base url that represents the filing folder
+                # Define a new base url
                 base_url = xml_summary.replace('FilingSummary.xml', '')
-
-                # Request and parse the content
-                time.sleep(.11)
-                for attempt in range(max_attempts):
-                    try:
-                        content = requests.get(xml_summary, headers=headers).content
-                        soup = BeautifulSoup(content, 'lxml')
-
-                        # Find the 'myreports' tag because this contains all the individual reports submitted.
-                        reports = soup.find('myreports')
-                        test = reports.find_all('report')[:-1]
-                    except:
-                        time.sleep(.25)
-                        continue
-                    else:
-                        break
-                else:
-                    tk.messagebox.showerror(title="Error", message="Network Error6. Please try again")
-                    sys.exit()
-               
-                # Loop through each report with the 'myreports' tag but avoid the last one as this will cause an error
-                for report in reports.find_all('report')[:-1]:
-                    if report.shortname.text.upper() in report_list:
-                        # Add Statement URL to List
-                        try:
-                            statements_url.append(base_url + report.htmlfilename.text)
-                        except:
-                            statements_url.append(base_url + report.xmlfilename.text)
                 break
 
-    # Create Data Set
-    statements_data = []
+        # Request and parse the content
+        time.sleep(.11)
+        for attempt in range(max_attempts):
+            try:
+                content = requests.get(xml_summary, headers=headers).content
+                soup = BeautifulSoup(content, 'lxml')
 
-    # Loop through statement URL's
-    for statement in statements_url:
-
-        # Dict to Store Statement
-        statement_data = {}
-        statement_data['headers'] = []
-        statement_data['sections'] = []
-        statement_data['data'] = []
+                # Find the all of the individual reports submitted
+                reports = soup.find('myreports')
+                test = reports.find_all('report')[:-1]
+            except:
+                time.sleep(.25)
+                continue
+            else:
+                break
+        else:
+            tk.messagebox.showerror(title="Error", message="Network Error6. Please try again")
+            sys.exit()
         
-        # Get Statement Data
-        content = requests.get(statement).content
-        report_soup = BeautifulSoup(content, 'html')
-
-        # Loop Through Rows in Content
-        for index, row in enumerate(report_soup.table.find_all('tr')):
+        # Loop through each report with the 'myreports' tag but avoid the last one as this will cause an error
+        for report in reports.find_all('report')[:-1]:
             
-            # Get All Elements
-            cols = row.find_all('td')
-            
-            # Check if Regular Row
-            if (len(row.find_all('th')) == 0 and len(row.find_all('strong')) == 0): 
-                reg_row = [ele.text.strip() for ele in cols]
-                statement_data['data'].append(reg_row)
-                
-            # Check if Section
-            elif (len(row.find_all('th')) == 0 and len(row.find_all('strong')) != 0):
-                sec_row = cols[0].text.strip()
-                statement_data['sections'].append(sec_row)
-                
-            # Check if Header
-            elif (len(row.find_all('th')) != 0):            
-                hed_row = [ele.text.strip() for ele in row.find_all('th')]
-                statement_data['headers'].append(hed_row)
-                
-            else:            
-                print('We encountered an error.')
+            # Summary table
+            if report.shortname.text.upper() in intro_list:
+                # Get URL and contents
+                try:
+                    intro_url = base_url + report.htmlfilename.text
+                    content = requests.get(intro_url, headers=headers).content
+                    soup = BeautifulSoup(content, 'html.parser')
+                    fy = fy_html(soup)
+                except:
+                    intro_url = base_url + report.xmlfilename.text
+                    content = requests.get(intro_url, headers=headers).content
+                    soup = BeautifulSoup(content, 'xml')
+                    fy = fy_xml(soup)
+        
+            # Income Statement
+            if report.shortname.text.upper() in income_list:
+                # Get URL and contents
+                try:
+                    rev_url = base_url + report.htmlfilename.text
+                    content = requests.get(rev_url, headers=headers).content
+                    soup = BeautifulSoup(content, 'html.parser')
+                    rev, gross, oi, net, eps = rev_html(soup)
+                except:
+                    rev_url = base_url + report.xmlfilename.text
+                    content = requests.get(rev_url, headers=headers).content
+                    soup = BeautifulSoup(content, 'xml')
+                    rev, gross, oi, net, eps = rev_xml(soup)
 
-        # Append to Statements Data
-        statements_data.append(statement_data)
+            # Balance sheet
+            if report.shortname.text.upper() in bs_list:
+                # Get URL and contents
+                try:
+                    bs_url = base_url + report.htmlfilename.text
+                    content = requests.get(bs_url, headers=headers).content
+                    soup = BeautifulSoup(content, 'html.parser')
+                    cash, assets, liabilities = bs_html(soup)
+                except:
+                    bs_url = base_url + report.xmlfilename.text
+                    content = requests.get(bs_url, headers=headers).content
+                    soup = BeautifulSoup(content, 'xml')
+
+            # Cash flow
+            if report.shortname.text.upper() in cf_list:
+                # Get URL and contents
+                try:
+                    cf_url = base_url + report.htmlfilename.text
+                    content = requests.get(cf_url, headers=headers).content
+                    soup = BeautifulSoup(content, 'html.parser')
+                    fcf, buyback, divpaid = cf_html(soup)
+                except:
+                    cf_url = base_url + report.xmlfilename.text
+                    content = requests.get(cf_url, headers=headers).content
+                    soup = BeautifulSoup(content, 'xml')
+            
+             # Dividends
+            if report.shortname.text.upper() in div_list:
+                # Get URL and contents
+                try:
+                    cf_url = base_url + report.htmlfilename.text
+                    content = requests.get(cf_url, headers=headers).content
+                    soup = BeautifulSoup(content, 'html.parser')
+                    div = div_html(soup)
+                except:
+                    cf_url = base_url + report.xmlfilename.text
+                    content = requests.get(cf_url, headers=headers).content
+                    soup = BeautifulSoup(content, 'xml')
+        
+        # Add parsed data to lists for data frame
+        Fiscal_Period.append(fy)
+        Revenue.append(rev)
+        Gross_Profit.append(gross)
+        Operating_Income.append(oi)
+        Net_Profit.append(net)
+        Earnings_Per_Share.append(eps)
+        Cash.append(cash)
+        Total_Assets.append(assets)
+        Total_Liabilities.append(liabilities)
+        Free_Cash_Flow.append(fcf)
+        Share_Buybacks.append(buyback)
+        Dividend_Payments.append(divpaid)
+        Dividends.append(div)
+
+        everything = [Fiscal_Period, Revenue, Gross_Profit, Operating_Income, Net_Profit, Earnings_Per_Share, Cash, Total_Assets, Total_Liabilities, Free_Cash_Flow, Share_Buybacks, Dividend_Payments, Dividends]
+        print(everything)
+
+
+
+
+
+
 
 
 
@@ -337,5 +537,5 @@ if /A filing is last, skip
 skip next filing if previous is amended (period of report?)
 pull quarter data from 10-k
 complete comment strings
-remove extra stuff from get filings (currently commented out)
+put everything into the stock class
 '''
