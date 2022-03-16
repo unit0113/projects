483a72b5c849d49e2fe8cc0d540edd326fcc7bac
Kyle Lund <unit0113@gmail.com>
2022-03-15 19:57:23 -0500

    Rotations on AVL complete, will need to replace insert fxn with recursive version

--- .venv/programs/Algorithms/Trees/avl.py
+++ .venv/programs/Algorithms/Trees/avl.py
@@ -5,8 +5,7 @@ class Node():
         self.parent = None
         self.left = None
         self.right = None
-        self.balance_factor = 0
-        self.height = 0
+        self.height = 1
 
 
 class AVL_Iterator():
@@ -25,26 +24,142 @@ class AVL_Iterator():
             raise StopIteration
 
 
-class AVL():
+class AVL_Tree():
     def __init__(self, *items):
         self.root = None
+        for key, value in items:
+            self.insert(key, value)
 
 
+    def __right_rotation__(self, old_root):
+        new_root = old_root.left
+        if new_root.right:
+            old_root.left = new_root.right
+            new_root.right.parent = old_root
+        
+        if old_root.parent:
+            if old_root.parent.left == old_root:
+                old_root.parent.left = new_root
+            else:
+                old_root.parent.right = new_root
+        else:
+            self.root = new_root
+
+        new_root.right = old_root
+        new_root.parent = old_root.parent
+        old_root.parent = new_root
+        old_root.height = 1 + max(self.__get_height__(old_root.right), self.__get_height__(old_root.left))
+        new_root.height = 1 + max(self.__get_height__(new_root.right), self.__get_height__(new_root.left))
+    
+    
+    def __left_rotation__(self, old_root):
+        new_root = old_root.right
+        if new_root.left:
+            old_root.right = new_root.left
+            new_root.left.parent = old_root
+        
+        if old_root.parent:
+            if old_root.parent.left == old_root:
+                old_root.parent.left = new_root
+            else:
+                old_root.parent.right = new_root
+        else:
+            self.root = new_root
+
+        new_root.left = old_root
+        new_root.parent = old_root.parent
+        old_root.parent = new_root
+        old_root.height = 1 + max(self.__get_height__(old_root.right), self.__get_height__(old_root.left))
+        new_root.height = 1 + max(self.__get_height__(new_root.right), self.__get_height__(new_root.left))
+    
+    
+    def __right_left_rotation__(self, old_root):
+        self.__right_rotation__(old_root.right)
+        self.__left_rotation__(old_root)
+    
+    
+    def __left_right_rotation__(self, old_root):
+        self.__left_rotation__(old_root.left)
+        self.__right_rotation__(old_root)
+
+
+    def __get_height__(self, root):
+        if not root:
+            return 0
+        
+        return root.height
+
+    
+    def __get_balance_factor__(self, root):
+        if not root:
+            return 0
+        
+        return self.__get_height__(root.left) - self.__get_height__(root.right)
+    
+    
     def insert(self, key, value):
         new_node = Node(key, value)
+        
         if not self.root:
             self.root = new_node
-        else:
-            cur_node = self.root
-            prev_node = None
-            while cur_node:
-                prev_node = cur_node
-                if new_node.key < cur_node.key:
-                    cur_node = cur_node.left
-                else:
-                    cur_node = cur_node.right
+            return
+        
+        cur_node = self.root
+        prev_node = None
+        while cur_node:
+            prev_node = cur_node
+            if new_node.key == cur_node.key:
+                cur_node.value = new_node.value
+                return
+            elif new_node.key < cur_node.key:
+                cur_node = cur_node.left
+            else:
+                cur_node = cur_node.right
 
-            
+        if new_node.key < cur_node.key:
+            prev_node.left = new_node
+        else:
+            prev_node.right = new_node
+        
+        new_node.parent = prev_node
+
+        balance = self.__get_balance_factor__(new_node)
+
+
+
+
+
+
+
+    def __transplant__(self, deleting_node, replacing_node=None):
+        if not deleting_node.parent:
+            self.root = replacing_node
+
+        elif deleting_node == deleting_node.parent.left:
+            deleting_node.parent.left = replacing_node
+        else:
+            deleting_node.parent.right = replacing_node
+
+        if replacing_node:
+            replacing_node.parent = deleting_node.parent
+
+
+
+
+    def __getitem__(self, key):
+        return self.search(key).value
+        #return getattr(self, key)        
+
+
+    def __setitem__(self, key, value):
+        setattr(self, key, value)            
+
+
+
+
+
+
+
 
 
     def delete(self, key):
@@ -97,3 +212,26 @@ class AVL():
             return True
         else:
             return False
+
+
+    def os_select(self, root, i):
+        rank = root.left.size + 1
+        if i == rank:
+            return root
+        elif i < rank:
+            return self.os_select(root.left, i)
+        else:
+            return self.os_select(root.right, i)
+
+    
+    def os_rank(self, key):
+        queried_node = self.search(key)
+        rank = queried_node.left.size + 1
+        
+        tmp_node = queried_node
+        while tmp_node != self.root:
+            if tmp_node.key == tmp_node.parent.right.key:
+                rank += tmp_node.parent.left.size + 1
+            tmp_node = tmp_node.parent
+        
+        return rank
--- .venv/programs/Algorithms/Trees/avl_test.py
+++ .venv/programs/Algorithms/Trees/avl_test.py
@@ -2,8 +2,88 @@ import unittest
 import avl as avl
 
 class TestLinkedList(unittest.TestCase):
+
+    def test_create_node(self):
+        a = avl.Node(5, 10)
+        self.assertEqual(10, a.value)
+        self.assertEqual(5, a.key)
+        self.assertEqual(None, a.parent)
+        self.assertEqual(None, a.left)
+        self.assertEqual(None, a.right)
+        self.assertEqual(1, a.height)
+
+    def test_left_rotate(self):
+        a = avl.AVL_Tree()
+        b = avl.Node(5, 5)
+        c = avl.Node(10, 10)
+        d = avl.Node(20,20)
+        a.root = b
+        b.right = c
+        c.parent = b
+        c.right = d
+        d.parent = c
+        a.__left_rotation__(b)
+        self.assertEqual(a.root, c)
+        self.assertEqual(c.left, b)
+        self.assertEqual(c.right, d)
+        self.assertEqual(b.parent, c)
+        self.assertEqual(d.parent, c)
+        e = avl.Node(30,30)
+        f = avl.Node(40,40)
+        g = avl.Node(25,25)
+        d.right = e
+        e.parent = d
+        e.right = f
+        e.left = g
+        g.parent = e
+        f.parent = e
+        a.__left_rotation__(d)
+        self.assertEqual(a.root, c)
+        self.assertEqual(d.parent, e)
+        self.assertEqual(e.left, d)
+        self.assertEqual(e.parent, c)
+        self.assertEqual(e.right, f)
+        self.assertEqual(f.parent, e)
+        self.assertEqual(g.parent, d)
+        self.assertEqual(d.right, g)
+
+    def test_right_rotate(self):
+        a = avl.AVL_Tree()
+        b = avl.Node(20, 20)
+        c = avl.Node(10, 10)
+        d = avl.Node(5, 5)
+        a.root = b
+        b.left = c
+        c.parent = b
+        c.left = d
+        d.parent = c
+        a.__right_rotation__(b)
+        self.assertEqual(a.root, c)
+        self.assertEqual(c.right, b)
+        self.assertEqual(c.left, d)
+        self.assertEqual(b.parent, c)
+        self.assertEqual(d.parent, c)
+        e = avl.Node(3,3)
+        f = avl.Node(2,2)
+        g = avl.Node(4,4)
+        d.left = e
+        e.parent = d
+        e.left = f
+        e.right = g
+        g.parent = e
+        f.parent = e
+        a.__right_rotation__(d)
+        self.assertEqual(a.root, c)
+        self.assertEqual(d.parent, e)
+        self.assertEqual(e.right, d)
+        self.assertEqual(e.parent, c)
+        self.assertEqual(e.left, f)
+        self.assertEqual(f.parent, e)
+        self.assertEqual(g.parent, d)
+        self.assertEqual(d.left, g)
+
     
-    def test_add_front_single(self):
+    '''def test_add_front_single(self):
         a = ll.Linked_List(5)
         a.add_front(10)
         self.assertEqual(10, a.head.value)
@@ -13,7 +93,7 @@ class TestLinkedList(unittest.TestCase):
         self.assertEqual(None, a.head.prev)
         self.assertEqual(10, a.tail.prev.value)
         with self.assertRaises(ValueError):
-            a.add_front()
+            a.add_front()'''
 
 
 if __name__ == '__main__':
