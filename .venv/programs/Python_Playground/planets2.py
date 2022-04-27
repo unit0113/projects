02c4e4f15d2f212dd1f1080275c9a545582b7f81
Kyle Lund <unit0113@gmail.com>
2022-04-26 20:46:45 -0500

    Major upgrades to planets V2, still can't get moons to work

--- .venv/programs/Python_Playground/planets2.py
+++ .venv/programs/Python_Playground/planets2.py
@@ -5,7 +5,7 @@ import random
 
 WIDTH, HEIGHT = 1400, 1400
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Planet Simulation")
+pygame.display.set_caption("Planet SimulationV2")
 
 # Colors
 RED = (255, 0, 0)
@@ -22,25 +22,33 @@ GREY = (128, 128, 128)
 TURQUOISE = (64, 224, 208)
 DARK_GRAY = (80, 78, 81)
 
-FONT = pygame.font.SysFont("comicsans", 16)
+FONT = pygame.font.SysFont('verdana', 14, bold=True)
 
 
-def random_start_location(distance):
-    M1 = 1.98892e30
+def random_start_location(distance, parent):
+    if parent:
+        x_parent = parent.x
+        y_parent = parent.y
+    else:
+        x_parent = y_parent = 0
 
     angle = 2 * math.pi * random.random()
-    x = distance * math.cos(angle)
-    y = distance * math.sin(angle)
+    x = x_parent + distance * math.cos(angle)
+    y = y_parent + distance * math.sin(angle)
 
-    if distance != 0:
-        velocity = math.sqrt(Planet.G * M1 / distance)
+    if parent:
+        velocity = math.sqrt(Planet.G * parent.mass / distance)
         vy = abs(velocity * math.cos(angle))
         if math.pi / 2 <= angle < 1.5 * math.pi:
             vy = vy * -1
 
+        vy += parent.y_vel
+
         vx = abs(velocity * math.sin(angle))
         if 0 <= angle < math.pi:
             vx = vx * -1
+
+        vx += parent.x_vel
         
     else:
         vx = vy = 0
@@ -51,25 +59,26 @@ def random_start_location(distance):
 class Planet:
     AU = 149.6e9
     G = 6.67428e-11
-    SCALE = 125 / AU # 1AU = 100 pixels
+    SCALE = 125 / AU 
     TIMESTEP = 3600 * 24 # 1 day per tick
 
-    def __init__(self, distance_to_sun, radius, color, mass, draw_orbit=True):
-        self.x, self.y, self.x_vel, self.y_vel = random_start_location(distance_to_sun * Planet.AU)
-        self.distance_to_sun = distance_to_sun
+    def __init__(self, name, distance_to_parent, radius, color, mass, *, parent = None, draw_orbit=True):
+        self.x, self.y, self.x_vel, self.y_vel = random_start_location(distance_to_parent * Planet.AU, parent)
+        self.name = name
+        self.distance_to_sun = distance_to_parent
         self.radius = radius
         self.color = color
         self.mass = mass
+        self.parent = parent
+        self.draw_orbit = draw_orbit
         self.orbit = []
 
 
-    def draw(self, window):
+    def draw(self, window, show_text=False):
         x = self.x * self.SCALE + WIDTH / 2
         y = self.y * self.SCALE + HEIGHT / 2
 
-        # Reorg with if draw_orbit
-
-        if len(self.orbit) > 2:
+        if self.draw_orbit and len(self.orbit) > 2:
             updated_points = []
             for point in self.orbit:
                 x, y = point
@@ -81,18 +90,20 @@ class Planet:
 
         pygame.draw.circle(window, self.color, (x, y), self.radius)
 		
-        if self.mass != 1.98892e30:
-            distance_text = FONT.render(f"{self.distance_to_sun:.4f}AU", 1, WHITE)
-            window.blit(distance_text, (x - distance_text.get_width()/2, y - distance_text.get_height()/2))
+        if show_text and self.draw_orbit:
+            name_text = FONT.render(self.name, 1, WHITE)
+            window.blit(name_text, (x - name_text.get_width()/2, y - name_text.get_height()/2 + self.radius + 10))
+            distance_text = FONT.render(f"{self.distance_to_parent:.4f}AU", 1, WHITE)
+            window.blit(distance_text, (x - distance_text.get_width()/2, y - distance_text.get_height()/2 + self.radius + 30))
 
 
-    def attraction(self, other):
-        distance_x = other.x - self.x
-        distance_y = other.y - self.y
+    def attraction(self):
+        distance_x = self.parent.x - self.x
+        distance_y = self.parent.y - self.y
         distance = math.sqrt(distance_x**2 + distance_y**2)
-        self.distance_to_sun = distance / Planet.AU
+        self.distance_to_parent = distance / Planet.AU
 
-        force = self.G * self.mass * other.mass / distance ** 2
+        force = self.G * self.mass * self.parent.mass / distance ** 2
         theta = math.atan2(distance_y, distance_x)
         force_x = force * math.cos(theta)
         force_y = force * math.sin(theta)
@@ -100,35 +111,89 @@ class Planet:
         return force_x, force_y
 
 
-    def update_position(self, sun):
-        fx, fy = self.attraction(sun)
+    def update_position(self):
+        if self.parent:
+            fx, fy = self.attraction()
 
-        self.x_vel += (fx / self.mass) * self.TIMESTEP
-        self.y_vel += (fy / self.mass) * self.TIMESTEP
+            self.x_vel += (fx / self.mass) * self.TIMESTEP
+            self.y_vel += (fy / self.mass) * self.TIMESTEP
 
-        self.x += self.x_vel * self.TIMESTEP
-        self.y += self.y_vel * self.TIMESTEP
-        self.orbit.append((self.x, self.y))
+            self.x += self.x_vel * self.TIMESTEP
+            self.y += self.y_vel * self.TIMESTEP
+            self.orbit.append((self.x, self.y))
 
 
 def display_date():
     pass
 
 
+def create_asteroid_belt(planets, num_asteroids, sun):
+    random_lst = [1] * 950 + [2] * 49 + [3] * 1
+
+    for _ in range(num_asteroids):
+        distance = random.gauss(2.67, 0.25)
+        radius = random.choice(random_lst)
+        mass = random.uniform(1e19, 1e21)
+        planets.append(Planet('Asteroid', distance, radius, DARK_GRAY, mass, parent=sun, draw_orbit=False))
+    
+    return planets
+
+
+class Toggle_Button(pygame.sprite.Sprite):
+    def __init__(self, scale, x, y):
+        super(Toggle_Button, self).__init__()
+        self.scale = scale
+        img_on = pygame.image.load('Python_Playground\\toggle1.png')
+        self.img_on = pygame.transform.scale(img_on, self.scale)
+        img_off = pygame.image.load('Python_Playground\\toggle2.png')
+        self.img_off = pygame.transform.scale(img_off, self.scale)
+        self.image = self.img_on
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.on = True
+
+
+    def update(self, pos):
+        if self.rect.collidepoint(pos):
+            self.on = not self.on
+            self.image = self.img_on if self.on else self.img_off
+
+        return self.on
+
+
+    def draw(self, win):
+        Toggle_text = FONT.render("Toggle Text", 1, WHITE)
+        win.blit(Toggle_text, (self.rect.x - Toggle_text.get_width() - 15, self.rect.y + 4))
+        win.blit(self.image, self.rect)
+
+
+def initialize_sim():
+    # Initialize objects
+    sun = Planet('Sun', 0, 20, YELLOW, 1.98892e30, draw_orbit=False)
+    mecury = Planet('Mecury', 0.387, 3, DARK_GRAY, 3.3e23, parent=sun)
+    venus = Planet('Venus', 0.723, 7, ORANGE, 4.8685e24, parent=sun)
+    earth = Planet('Earth', 1, 8, LTBLUE, 5.9742e24, parent=sun)
+    luna = Planet('Luna', .002569, 2, GREY, 7.34767309e22, parent=earth, draw_orbit=False)
+    mars = Planet('Mars', 1.524, 6, LTRED, 6.39e23, parent=sun)
+    jupiter = Planet('Jupiter', 5.2, 15, TURQUOISE, 1.89813e27, parent=sun)
+
+    planets = [sun, mecury, venus, earth, luna, mars, jupiter]
+
+    # Initilize asteroid belt
+    planets = create_asteroid_belt(planets, 5000, sun)
+
+    return planets
+
 def main():
     run = True
     clock = pygame.time.Clock() # Allows simulation to run at set speed rather than speed of computer
 
-    # Initialize objects
-    sun = Planet(0, 20, YELLOW, 1.98892e30)
+    planets = initialize_sim()
 
-    mecury = Planet(0.387, 3, DARK_GRAY, 3.3e23)
-    venus = Planet(0.723, 7, ORANGE, 4.8685e24)
-    earth = Planet(1, 8, LTBLUE, 5.9742e24)
-    mars = Planet(1.524, 6, LTRED, 6.39e23)
-    jupiter = Planet(5.2, 15, TURQUOISE, 1.89813e27)
-
-    planets = [sun, mecury, venus, earth, mars, jupiter]
+    # Text toggle button
+    toggle1_btn = Toggle_Button((50,30), 1325, 25)
+    show_text = True
 
     while run:
         clock.tick(60) # Update at 60 FPS
@@ -139,13 +204,28 @@ def main():
                 run = False
                 break
 
-        for planet in planets:
-            if planet is not sun:
-                planet.update_position(sun)
-            planet.draw(WINDOW)
+            if pygame.mouse.get_pressed()[0]:
+                pos = pygame.mouse.get_pos()
+                show_text = toggle1_btn.update(pos)
 
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_c:
+                planets = initialize_sim()
+
+        for planet in planets:
+            planet.update_position()
+            planet.draw(WINDOW, show_text)
+
+        toggle1_btn.draw(WINDOW)
         pygame.display.update()
 
     pygame.quit()
 
-main()
+if __name__ == "__main__":
+    main()
+
+
+"""TODO
+Make button not look terrible
+add moons
+Display simulated date
+"""
