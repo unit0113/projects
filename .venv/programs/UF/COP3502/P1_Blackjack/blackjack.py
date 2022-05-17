6c29bdc7d37036918ec800101afe337b99860a01
Kyle Lund <unit0113@gmail.com>
2022-05-17 07:28:41 -0500

    3502 P1 fixed, dumbed down and submitted

--- .venv/programs/UF/COP3502/P1_Blackjack/blackjack.py
+++ .venv/programs/UF/COP3502/P1_Blackjack/blackjack.py
@@ -50,47 +50,21 @@ class Game:
         print()
 
 
-    def draw_card(self):
+    def draw_card(self, hand):
         """Draw a card, print the result, and return the value of the card
 
         Returns:
             int: hand value of the drawn card
         """
-        card_names = [None, 'ACE'] + [str(num) for num in range(2, 11)] + ['Jack', 'Queen', 'King']
+        card_names = [None, 'ACE'] + [str(num) for num in range(2, 11)] + ['JACK', 'QUEEN', 'KING']
         card_values = [None] + [num for num in range(1, 11)] + [10] * 3
 
         # Retrieve and print card with proper name
         card = RNG.next_int(13) + 1
         print(f'Your card is a {card_names[card]}!')
+        self.print_hand(hand + card_values[card])
 
         return card_values[card]
-
-
-    def draw_card_AI(self):
-        """Draw a card and return the value of the card. AI version does not print anything
-
-        Returns:
-            int: hand value of the drawn card
-        """
-        card_values = [None] + [num for num in range(1, 11)] + [10] * 3
-
-        # Retrieve the card
-        card = RNG.next_int(13) + 1
-
-        return card_values[card]
-
-
-    def play_dealer(self):
-        """Play the dealers hand. Must hit when less than 17
-
-        Returns:
-            int: Dealer hand value
-        """
-        hand = 0
-        while hand < 17:
-            hand += self.draw_card_AI()
-
-        return hand
 
 
     def print_hand(self, hand):
@@ -166,14 +140,13 @@ class Game:
         print()
 
         # Initial card draw
-        hand = self.draw_card()
+        hand = self.draw_card(0)
 
         # Player turn
         while True:
-            self.print_hand(hand)
             response = self.menu()
             if response == 1:
-                hand += self.draw_card()
+                hand += self.draw_card(hand)
             elif response == 2:
                 break
             elif response == 3:
@@ -184,7 +157,7 @@ class Game:
             self.check_early_end(hand)
 
         # Play dealer
-        dealer_hand = self.play_dealer()
+        dealer_hand = RNG.next_int(11) + 16
 
         # Determine winner
         self.endgame(hand, dealer_hand)
