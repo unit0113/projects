6396d6f0cb75fbc0f8bfe12d72b59955066d12ae
Kyle Lund <unit0113@gmail.com>
2022-10-28 18:24:09 -0500

    Support vector regressor walkthrough

--- Udemy/Machine Learning A-Z/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/Python/support_vector_regression.py
+++ Udemy/Machine Learning A-Z/Part 2 - Regression/Section 7 - Support Vector Regression (SVR)/Python/support_vector_regression.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
+dataset = pd.read_csv(r"Udemy\Machine Learning A-Z\Part 2 - Regression\Section 7 - Support Vector Regression (SVR)\Python\Position_Salaries.csv")
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 print(X)
