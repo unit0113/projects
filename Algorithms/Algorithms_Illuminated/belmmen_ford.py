25887e7f85ea709a2a35513999aa2a96fb2c1a20
Kyle Lund <unit0113@gmail.com>
2023-01-01 16:23:02 -0600

    Bellman Ford

---
+++ Algorithms/Algorithms_Illuminated/belmmen_ford.py
@@ -0,0 +1,46 @@
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
+
+    def addEdge(self, u, v, w):
+        self.graph.append((u, v, w))
+
+    def printArr(self, dist):
+        print("Vertex Distance")
+        for i in range(self.V):
+            print("{0}\t{1}".format(i, dist[i]))
+ 
+    def BellmanFord(self, src):
+        # Step 1: Initialize distances from src
+        dist = [float("Inf")] * self.V
+        dist[src] = 0
+ 
+        # Relax all edges
+        for _ in range(self.V - 1):
+            # Update dist value and parent index of the adjacent vertices
+            for u, v, w in self.graph:
+                if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                    dist[v] = dist[u] + w
+ 
+        # Check for negative-weight cycles 
+        for u, v, w in self.graph:
+            if dist[u] != float("Inf") and dist[u] + w < dist[v]:
+                print("Graph contains negative weight cycle")
+                return
+
+        self.printArr(dist)
+ 
+
+if __name__ == '__main__':
+    g = Graph(5)
+    g.addEdge(0, 1, -1)
+    g.addEdge(0, 2, 4)
+    g.addEdge(1, 2, 3)
+    g.addEdge(1, 3, 2)
+    g.addEdge(1, 4, 2)
+    g.addEdge(3, 2, 5)
+    g.addEdge(3, 1, 1)
+    g.addEdge(4, 3, -3)
+
+    g.BellmanFord(0)
