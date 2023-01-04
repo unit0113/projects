986cdf60052122fd4da5479fe66ac97293fd8a54
Kyle Lund <unit0113@gmail.com>
2023-01-03 18:57:43 -0600

    Bellman held karp

---
+++ Algorithms/Algorithms_Illuminated/bellman_held_karp.py
@@ -0,0 +1,78 @@
+import itertools
+import random
+import sys
+
+
+def held_karp(dists):
+    """
+    Implementation of Held-Karp, an algorithm that solves the Traveling
+    Salesman Problem using dynamic programming with memoization.
+    Parameters: dists: distance matrix
+    Returns: A tuple, (cost, path).
+    """
+    n = len(dists)
+
+    # Maps each subset of the nodes to the cost to reach that subset, as well
+    # as what node it passed before reaching this subset.
+    # Node subsets are represented as set bits.
+    C = {}
+
+    # Set transition cost from initial state
+    for k in range(1, n):
+        C[(1 << k, k)] = (dists[0][k], 0)
+
+    # Iterate subsets of increasing length and store intermediate results in classic dynamic programming manner
+    for subset_size in range(2, n):
+        for subset in itertools.combinations(range(1, n), subset_size):
+            # Set bits for all nodes in this subset
+            bits = 0
+            for bit in subset:
+                bits |= 1 << bit
+
+            # Find the lowest cost to get to this subset
+            for k in subset:
+                prev = bits & ~(1 << k)
+
+                res = []
+                for m in subset:
+                    if m == 0 or m == k:
+                        continue
+                    res.append((C[(prev, m)][0] + dists[m][k], m))
+                C[(bits, k)] = min(res)
+
+    # We're interested in all bits but the least significant (the start state)
+    bits = (2 ** n - 1) - 1
+
+    # Calculate optimal cost
+    res = []
+    for k in range(1, n):
+        res.append((C[(bits, k)][0] + dists[k][0], k))
+    opt, parent = min(res)
+
+    # Backtrack to find full path
+    path = []
+    for _ in range(n - 1):
+        path.append(parent)
+        new_bits = bits & ~(1 << parent)
+        _, parent = C[(bits, parent)]
+        bits = new_bits
+
+    # Add implicit start state
+    path.append(0)
+
+    return opt, list(reversed(path))
+
+
+if __name__ == '__main__':
+    dists = [[0,  2,  9, 10],
+             [1,  0,  6,  4],
+             [15, 7,  0,  8],
+             [6,  3, 12,  0]
+             ]
+
+    # Pretty-print the distance matrix
+    for row in dists:
+        print(''.join([str(n).rjust(3, ' ') for n in row]))
+
+    print()
+    print(held_karp(dists)) # 21
