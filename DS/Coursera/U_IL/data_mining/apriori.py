d9f9fdc96a371143b1db80ad2825f18f7aba6c83
Kyle Lund <unit0113@gmail.com>
2023-03-02 18:56:31 -0600

    Apriori complete

--- DS/Coursera/U_IL/data_mining/apriori.py
+++ DS/Coursera/U_IL/data_mining/apriori.py
@@ -1,14 +1,13 @@
 import os
 import sys
 from collections import defaultdict
-
+from itertools import combinations
 
 MIN_SUPPORT = 0.01
 
 
 def read_data() -> tuple[defaultdict[str, int], int]:
-    file_name = 'categories.txt'
-    path = os.path.join(sys.path[0], file_name)
+    path = os.path.join(sys.path[0], 'categories.txt')
 
     category_dict = defaultdict(lambda: 0)
     category_count = 0
@@ -34,22 +33,51 @@ def get_frequent(data_dict: defaultdict[str, int], count: int) -> defaultdict[st
 
 
 def part1(data_dict: defaultdict[str, int], count: int) -> None:
-    support_dict = get_frequent(data_dict, count)
-    print_to_file(support_dict, 'patterns.txt')
-
-
-def print_to_file(data_dict: dict[str, int], file_name: str) -> None:
-    path = os.path.join(sys.path[0], file_name)
+    frequent_dict = get_frequent(data_dict, count)
+    path = os.path.join(sys.path[0], 'patterns.txt')
 
     with open(path, 'w') as file:
-        for catgegory, support in reversed(sorted(data_dict.items(), key=lambda kv: kv[1])):
+        for catgegory, support in reversed(sorted(frequent_dict.items(), key=lambda kv: kv[1])):
             file.write(f"{support}:{catgegory}\n")
 
 
+def part2(data_dict: defaultdict[str, int], count: int) -> None:
+    frequent_dict = get_frequent(data_dict, count)
+    read_path = os.path.join(sys.path[0], 'categories.txt')
+    write_path = os.path.join(sys.path[0], 'patterns.txt')
+
+    n = 2
+    while frequent_dict:
+        category_dict = defaultdict(lambda: 0)
+        combos = get_combinations(frequent_dict, n)
+        with open(read_path, 'r') as file:
+            for line in file.readlines():
+                line_list = line.strip().split(';')
+                for item_set in combos:
+                    if all(cat in line_list for cat in item_set):
+                        category_dict[item_set] += 1
+        n += 1
+        frequent_dict = get_frequent(category_dict, count)
+        with open(write_path, 'a') as file:
+            for catgegories, support in reversed(sorted(frequent_dict.items(), key=lambda kv: kv[1])):
+                file.write(f"{support}:{';'.join(catgegories)}\n")
 
 
+def get_combinations(frequent_dict: defaultdict[str, int], length):
+    if length == 2:
+        return list(combinations(frequent_dict.keys(), 2))
+    
+    combos = []
+    for index, set1_keys in enumerate(frequent_dict.keys()):
+        for set2_keys in list(frequent_dict.keys())[index + 1:]:
+            combo = tuple(sorted(set(set1_keys + set2_keys)))
+            if len(combo) == length and combo not in combos:
+                combos.append(combo)
+
+    return combos
 
 
 if __name__ == '__main__':
     data_dict, count = read_data()
     part1(data_dict, count)
+    part2(data_dict, count)
