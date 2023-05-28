d41125d5d59604578709d67c70a4aaf1c7650ca1
Kyle Lund <unit0113@gmail.com>
2023-05-27 20:49:12 -0500

    AVL project progress

--- UF/COP3530/Project1/src/main.cpp
+++ UF/COP3530/Project1/src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <memory>
 #include <string>
+#include <queue>
 
 using namespace std;
 
@@ -13,29 +13,35 @@ struct Node {
 	string name;
 	unsigned int ID;
 	unsigned int height = 1;
-	shared_ptr<Node> parent = nullptr;
-	shared_ptr<Node> l_child = nullptr;
-	shared_ptr<Node> r_child = nullptr;
+	Node* parent = nullptr;
+	Node* l_child = nullptr;
+	Node* r_child = nullptr;
 
-	Node(string new_name, unsigned int new_ID, shared_ptr<Node> new_parent=nullptr) : name(new_name), ID(new_ID), parent(new_parent) {}
+	Node(string new_name, unsigned int new_ID, Node* new_parent=nullptr) : name(new_name), ID(new_ID), parent(new_parent) {}
 };
 
 
 class AVL_Tree {
 	private:
-		shared_ptr<Node> head = nullptr;
-		shared_ptr<Node> insert_helper(string name, unsigned int ID, shared_ptr<Node> root);
-		unsigned int get_height(shared_ptr<Node> root);
-		shared_ptr<Node> rebalance(shared_ptr<Node> root);
-		unsigned int get_balance_factor(shared_ptr<Node> root);
-		shared_ptr<Node> left_rotation(shared_ptr<Node> old_root);
-		shared_ptr<Node> right_rotation(shared_ptr<Node> old_root);
+		Node* head = nullptr;
+
+		// Helper methods
+		Node* insert_helper(string name, unsigned int ID, Node* root);
+		unsigned int get_height(Node* root);
+		Node* rebalance(Node* root);
+		unsigned int get_balance_factor(Node* root);
+		Node* left_rotation(Node* old_root);
+		Node* right_rotation(Node* old_root);
+		void in_order_helper(Node* root, queue<string>& names) const;
+		void print_helper(queue<string>& names) const;
+		void pre_order_helper(Node* root, queue<string>& names) const;
+		void post_order_helper(Node* root, queue<string>& names) const;
 
 	public:
 		void insert(string name, unsigned int ID);
-		void remove(const unsigned int& ID);
-		void search(const unsigned int& ID) const;
-		void search(const string& name) const;
+		void remove(const unsigned int& remove_ID);
+		void search(const unsigned int& search_ID) const;
+		void search(const string& search_name) const;
 		void printInOrder() const;
 		void printPreOrder() const;
 		void printPostOrder() const;
@@ -47,7 +53,7 @@ class AVL_Tree {
 void AVL_Tree::insert(string name, unsigned int ID) {
 	// For empty tree
 	if (head == nullptr) {
-		head = make_shared<Node>(name, ID);
+		head = new Node(name, ID);
 		cout << "successful" << endl;
 		return;
 	}
@@ -57,15 +63,15 @@ void AVL_Tree::insert(string name, unsigned int ID) {
 }
 
 
-shared_ptr<Node> AVL_Tree::insert_helper(string name, unsigned int ID, shared_ptr<Node> root) {
+Node* AVL_Tree::insert_helper(string name, unsigned int ID, Node* root) {
 	// Base case for reaching end of tree
 	if (root == nullptr) {
 		cout << "successful" << endl;
-		return make_shared<Node>(name, ID);
+		return new Node(name, ID);
 	}
 	// Go to left
 	if (ID < root->ID)  {
-		shared_ptr<Node> left_sub_root = insert_helper(name, ID, root->l_child);
+		Node* left_sub_root = insert_helper(name, ID, root->l_child);
 		root->l_child = left_sub_root;
 		left_sub_root->parent = root;
 	}
@@ -78,7 +84,7 @@ shared_ptr<Node> AVL_Tree::insert_helper(string name, unsigned int ID, shared_pt
 
 	// Go right
 	else {
-		shared_ptr<Node> right_sub_root = insert_helper(name, ID, root->r_child);
+		Node* right_sub_root = insert_helper(name, ID, root->r_child);
 		root->r_child = right_sub_root;
 		right_sub_root->parent = root;
 	}
@@ -91,13 +97,13 @@ shared_ptr<Node> AVL_Tree::insert_helper(string name, unsigned int ID, shared_pt
 }
 
 
-unsigned int AVL_Tree::get_height(shared_ptr<Node> root) {
+unsigned int AVL_Tree::get_height(Node* root) {
 	if (root == nullptr) {return 0;}
 	return root->height;
 }
 
 
-shared_ptr<Node> AVL_Tree::rebalance(shared_ptr<Node> root) {
+Node* AVL_Tree::rebalance(Node* root) {
 	int balance_factor = get_balance_factor(root);
 
 	// Right or left-right rotation
@@ -120,14 +126,14 @@ shared_ptr<Node> AVL_Tree::rebalance(shared_ptr<Node> root) {
 }
 
 
-unsigned int AVL_Tree::get_balance_factor(shared_ptr<Node> root) {
+unsigned int AVL_Tree::get_balance_factor(Node* root) {
 	if (root == nullptr) {return 0;}
 	return get_height(root->l_child) - get_height(root->r_child);
 }
 
 
-shared_ptr<Node> AVL_Tree::left_rotation(shared_ptr<Node> old_root) {
-	shared_ptr<Node> new_root = old_root->r_child;
+Node* AVL_Tree::left_rotation(Node* old_root) {
+	Node* new_root = old_root->r_child;
 	old_root->r_child = new_root->l_child;
 
 	// Assign parent to left child of new root
@@ -161,8 +167,8 @@ shared_ptr<Node> AVL_Tree::left_rotation(shared_ptr<Node> old_root) {
 }
 
 
-shared_ptr<Node> AVL_Tree::right_rotation(shared_ptr<Node> old_root) {
-	shared_ptr<Node> new_root = old_root->l_child;
+Node* AVL_Tree::right_rotation(Node* old_root) {
+	Node* new_root = old_root->l_child;
 	old_root->l_child = new_root->r_child;
 
 	// Assign parent to left child of new root
@@ -196,6 +202,117 @@ shared_ptr<Node> AVL_Tree::right_rotation(shared_ptr<Node> old_root) {
 }
 
 
+void AVL_Tree::remove(const unsigned int& remove_ID) {
+
+}
+
+
+void AVL_Tree::search(const unsigned int& search_ID) const {
+	Node* curr = head;
+	while (curr) {
+		if (curr->ID == search_ID) {
+			cout << curr->name << endl;
+			return;
+		}
+		curr = (search_ID < curr->ID) ? curr->l_child : curr->r_child;
+	}
+	cout << "unsuccessful" << endl;
+}
+
+
+void AVL_Tree::search(const string& search_name) const {
+
+}
+
+
+void AVL_Tree::printInOrder() const {
+	queue<string> names;
+	in_order_helper(head, names);
+	print_helper(names);
+}
+
+
+void AVL_Tree::in_order_helper(Node* root, queue<string>& names) const {
+	if (root) {
+		in_order_helper(root->l_child, names);
+		names.push(root->name);
+		in_order_helper(root->r_child, names);
+	}
+}
+
+
+void AVL_Tree::printPreOrder() const {
+	queue<string> names;
+	pre_order_helper(head, names);
+	print_helper(names);
+}
+
+
+void AVL_Tree::pre_order_helper(Node* root, queue<string>& names) const {
+	if (root) {
+		names.push(root->name);
+		in_order_helper(root->l_child, names);
+		in_order_helper(root->r_child, names);
+	}
+}
+
+		
+void AVL_Tree::printPostOrder() const {
+	queue<string> names;
+	post_order_helper(head, names);
+	print_helper(names);
+}
+
+
+void AVL_Tree::post_order_helper(Node* root, queue<string>& names) const {
+	if (root) {
+		in_order_helper(root->l_child, names);
+		in_order_helper(root->r_child, names);
+		names.push(root->name);
+	}
+}
+
+
+void AVL_Tree::print_helper(queue<string>& names) const {
+	cout << names.front();
+	names.pop();
+	while (!names.empty()) {
+		cout << ", " << names.front();
+		names.pop();
+	}
+	cout << endl;
+}
+
+
+void AVL_Tree::printLevelCount() const {
+	if (!head) {
+		cout << 0 << endl;
+	}
+	cout << head->height << endl;
+}
+
+
+void AVL_Tree::removeInOrder(unsigned int N) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -203,5 +320,11 @@ shared_ptr<Node> AVL_Tree::right_rotation(shared_ptr<Node> old_root) {
 int main(){
 	AVL_Tree tree;
 	tree.insert("Bob", 42);
+	tree.insert("Alice", 25);
+	tree.insert("Charlie", 72);
+	tree.printInOrder();
+	tree.printPreOrder();
+	tree.printPostOrder();
+	tree.printLevelCount();
 }
 
