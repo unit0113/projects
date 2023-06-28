a859463bbd3fc648582387d6798990fd46cd837e
Kyle Lund <unit0113@gmail.com>
2023-06-27 20:10:55 -0500

    Programming assignment 6 and bridges EC complete

--- UF/COP3530/Module6/extract_max/src/extract_max.h
+++ UF/COP3530/Module6/extract_max/src/extract_max.h
@@ -9,25 +9,57 @@
   Sample Input:
     3
     9 8 7
-  
+
   Sample Output:
     8 7
-		
-	Explanation:
+    
+  Explanation:
 
-	Input:  Line 1 denotes the number of elements, size in the 
+  Input:  Line 1 denotes the number of elements, size in the 
             heap. Line 2 denotes the contents of the max heap 
             that is passed into extractMax() function.
-	
-	Output: Output is the max heap after deletion.
+
+  Output: Output is the max heap after deletion.
 */
 
 #include <iostream>
+#include <algorithm>
 
 int* extractMax(int arr[], int size)
 {
-  // code here
-	// main prints the final heap array from 0 to size - 2
-	// you don't need to print the array but instead return
-	return arr; 
+
+    // code here
+    // main prints the final heap array from 0 to size - 2
+    // you don't need to print the array but instead return
+    arr[0] = arr[--size];
+
+    // Initialize helper variables
+    int index{};
+    int left{};
+    int right{};
+    int largest{};
+
+    // Heapify down
+    while (index < size) {
+        left = 2 * index + 1;
+        right = 2 * index + 2;
+        largest = index;
+        
+        // Find index of largest between parent and children
+        if (left < size && arr[left] > arr[largest]) {
+            largest = left;
+        }
+        if (right < size && arr[right] > arr[largest]) {
+            largest = right;
+        }
+                                          
+        if (largest == index) {
+            break;
+        }
+        
+        std::swap(arr[index], arr[largest]);
+        index = largest;       
+  }
+
+    return arr; 
 }
