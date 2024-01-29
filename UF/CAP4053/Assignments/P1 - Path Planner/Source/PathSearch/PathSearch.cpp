85efe008ca8b8f2d4e371e1bcbdd1fc0c391f551
Kyle Lund <unit0113@gmail.com>
2024-01-28 19:26:01 -0600

    Initial commit of P1

--- UF/CAP4053/Assignments/P1 - Path Planner/Source/PathSearch/PathSearch.cpp
+++ UF/CAP4053/Assignments/P1 - Path Planner/Source/PathSearch/PathSearch.cpp
@@ -1,7 +1,171 @@
+#include "PathSearch.h"
+#include <chrono>
+
 namespace ufl_cap4053
 {
+	namespace constants {
+		const std::unordered_map<int, std::unordered_set<std::pair<int, int>>> ADJACENT_TILES = { {0, {{-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, 0}, {0, 1}}}, {1, {{0,-1}, {-1, 0}, {0, 1}, {1, -1}, {1, 0}, {1, 1}}} };
+		const double HEURISTIC_WEIGHT = 1.0f;
+	}
+
 	namespace searches
 	{
-		// CLASS DEFINITION GOES HERE
+		PlannerNode::PlannerNode(const Tile* tile, const Tile* parent = nullptr, double gCost = 0, double hCost = 0) : tile(tile), parent(parent), givenCost(gCost), heuristicCost(hCost) {
+			\
+				finalCost = givenCost + constants::HEURISTIC_WEIGHT * heuristicCost;
+		};
+
+		PathSearch::~PathSearch() {}
+
+		//! \brief Called after the tile map is loaded. Generates the search graph
+		void PathSearch::load(TileMap* _tileMap) {
+			tileMap = _tileMap;
+			std::unordered_set<std::pair<int, int>> posNeighbors{};
+			std::pair<int, int > neighbor{};
+			Tile* keyTile{};
+			Tile* neighborTile{};
+
+			int rows = _tileMap->getRowCount();
+			int cols = _tileMap->getColumnCount();
+
+			// Iterate over rows
+			for (int row{}; row < rows; ++row) {
+				// Iterate over cols
+				for (int col{}; col < cols; ++col) {
+					keyTile = _tileMap->getTile(row, col);
+					// Skip if no tile exists at location
+					if (!keyTile) {
+						continue;
+					}
+					// Initialize tile pair and get relative neighbor indexes
+					posNeighbors = constants::ADJACENT_TILES.at(row % 2);
+					// Check each posible neighbor
+					for (const std::pair<int, int>& move : posNeighbors) {
+						// Convert relative movement to row/col in tilemap
+						neighbor = std::make_pair(row + move.first, col + move.second);
+						// Check if valid position (possibly unnecessary)
+						if ((0 <= neighbor.first) && (0 <= neighbor.second) && (neighbor.first < rows) && (neighbor.second < cols)) {
+							neighborTile = _tileMap->getTile(neighbor.first, neighbor.second);
+							// Add as neighbor in search graph if tile exists
+							if (neighborTile) {
+								searchGraph[keyTile].insert(neighborTile);
+							}
+						}
+					}
+				}
+			}
+
+		}
+
+		//! \brief Called before any update of the path planner,
+		//! prepares for search to be performed between the tiles at
+		//! the coordinates indicated.This method is always preceded by at least one call to load().
+		void PathSearch::initialize(int startRow, int startCol, int goalRow, int goalCol) {
+			startTile = tileMap->getTile(startRow, startCol);
+			goalTile = tileMap->getTile(goalRow, goalCol);
+			isComplete = false;
+			open.push(new PlannerNode(startTile));
+			solution.clear();
+		}
+
+		//! \brief Runs path planner for the specified timeslice.
+		//! If timeslice is 0, will run once. Otherwise, 
+		//! will run until time elapses
+		void PathSearch::update(long timeslice) {
+			if (timeslice == 0) {
+				aStarIteration();
+				return;
+			}
+
+			auto stopPoint = std::chrono::system_clock::now().time_since_epoch() + std::chrono::milliseconds(timeslice);
+			auto endIter = std::chrono::system_clock::now().time_since_epoch();
+			auto startIter = std::chrono::system_clock::now().time_since_epoch();
+			auto maxIterDuration = endIter - startIter;
+			while ((endIter + maxIterDuration < stopPoint) && !isComplete) {
+				startIter = std::chrono::system_clock::now().time_since_epoch();
+				aStarIteration();
+				endIter = std::chrono::system_clock::now().time_since_epoch();
+				maxIterDuration = max(maxIterDuration, endIter - startIter);
+			}
+		}
+
+		//! \brief Cleans up memory for this search
+		void PathSearch::shutdown() {
+			visited.clear();
+			open.clear();
+		}
+
+		//! \brief Cleans up memory associated with the tile map
+		void PathSearch::unload() {
+			searchGraph.clear();
+		}
+
+		//! \brief Returns true if a solution has been found
+		//! and false otherwise.
+		bool PathSearch::isDone() const {
+			return isComplete;
+		}
+
+		std::vector<Tile const*> const PathSearch::getSolution() const {
+			return solution;
+		}
+
+		// \brief Runs a single iteration of A* search
+		void PathSearch::aStarIteration() {
+			if (!open.empty()) {
+				// Retrieve next node
+				PlannerNode* current = open.front();
+				open.pop();
+
+				// Check if goal
+				if (current->getTile() == goalTile) {
+					isComplete = true;
+					buildSolution();
+					return;
+				}
+
+				double cost{};
+				PlannerNode* next;
+				for (const Tile* neighbor : searchGraph.at(current->getTile())) {
+					cost = current->getGivenCost() + neighbor->getWeight() * tileMap->getTileRadius();
+					// Check if visited
+					try {
+						// Replace node if better path found
+						next = visited.at(neighbor);
+						if (cost < next->getGivenCost()) {
+							open.remove(next);
+							next->setGivenCost(cost);
+							next->setParent(current->getTile());
+							open.push(next);
+						}
+					}
+					// If not found
+					catch (const std::out_of_range& e) {
+						next = new PlannerNode(neighbor, current->getTile(), cost, heuristic(neighbor));
+						open.push(next);
+						visited.at(neighbor) = next;
+					}
+				}
+			}
+		}
+
+		void PathSearch::buildSolution() {
+			const Tile* current = goalTile;
+			while (current) {
+				solution.push_back(current);
+				current = visited.at(current)->getParent();
+			}
+		}
+
+		bool PathSearch::areAdjacent(const Tile* lhs, const Tile* rhs) const {
+			// Find relative distance
+			std::pair<int, int> dist = { lhs->getRow() - rhs->getRow(), lhs->getColumn() - rhs->getColumn() };
+			
+			return constants::ADJACENT_TILES.at(lhs->getRow() % 2).find(dist) != constants::ADJACENT_TILES.at(lhs->getRow() % 2).end();
+		}
+
+		int PathSearch::heuristic(const Tile* src) const {
+			return std::abs(src->getRow() - goalTile->getRow()) + std::abs(src->getColumn() - goalTile->getColumn());
+		}
 	}
 }  // close namespace ufl_cap4053::searches
--- UF/CAP4053/Assignments/P1 - Path Planner/Source/PathSearch/PathSearch.h
+++ UF/CAP4053/Assignments/P1 - Path Planner/Source/PathSearch/PathSearch.h
@@ -1,14 +1,64 @@
 #include "../platform.h" // This file will make exporting DLL symbols simpler for students.
+#include "../../Source/Framework/TileSystem/Tile.h"
+#include "../../Source/Framework/TileSystem/TileMap.h"
+#include "../PriorityQueue.h"
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
 
 namespace ufl_cap4053
 {
 	namespace searches
 	{
-		class PathSearch
-		{
-		// CLASS DECLARATION GOES HERE
-			public:
-				DLLEXPORT PathSearch(); // EX: DLLEXPORT required for public methods - see platform.h
+		class PlannerNode {
+		public:
+			PlannerNode(const Tile* tile, const Tile* parent = nullptr, double gCost = 0, double hCost = 0) : tile(tile), parent(parent), givenCost(gCost), heuristicCost(hCost) { finalCost = givenCost + heuristicCost; };
+			bool operator == (const PlannerNode& other) { return other.tile == tile; };
+			const Tile* getTile() const { return tile; };
+			void setParent(const Tile* newParent) { parent = newParent; };
+			const Tile* getParent() const { return parent; };
+			double getGivenCost() const { return givenCost; };
+			void setGivenCost(double newCost) { givenCost = newCost; };
+			double getFinalCost() const { return finalCost; };
+
+
+		private:
+			const Tile* tile;
+			const Tile* parent;
+			double givenCost;
+			double heuristicCost;
+			double finalCost;
+
+		};
+
+		class PathSearch {
+		public:
+			DLLEXPORT PathSearch() : open(greaterThan) {};
+			DLLEXPORT ~PathSearch();
+			DLLEXPORT void load(TileMap* _tileMap); //Creates search graph
+			DLLEXPORT void initialize(int startRow, int startCol, int goalRow, int goalCol); //Called before update of path planner
+			DLLEXPORT void update(long timeslice); //Execute path planner
+			DLLEXPORT void shutdown(); //Cleans up memory for this search
+			DLLEXPORT void unload(); //Cleans up memory for tile map
+			DLLEXPORT bool isDone() const; //If solution found
+			DLLEXPORT std::vector<Tile const*> const getSolution() const;
+
+		private:
+			TileMap* tileMap;
+			std::unordered_map<const Tile*, std::unordered_set<const Tile*>> searchGraph;
+			std::unordered_map<const Tile*, PlannerNode*> visited;
+			const Tile* startTile;
+			const Tile* goalTile;
+			bool isComplete;
+			PriorityQueue<PlannerNode*> open;
+			std::vector<Tile const*> solution{};
+
+			void aStarIteration();
+			void buildSolution();
+			bool areAdjacent(const Tile* lhs, const Tile* rhs) const;
+			int heuristic(const Tile* src) const;
+			bool greaterThan(PlannerNode* const& lhs, PlannerNode* const& rhs) { return lhs->getFinalCost() > rhs->getFinalCost(); };
 		};
 	}
 }  // close namespace ufl_cap4053::searches
